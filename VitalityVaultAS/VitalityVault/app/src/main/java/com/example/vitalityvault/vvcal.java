c5031b50b61c17542757f7fca6530f0c345f3f42
Conor Griffin <115548358+YoungGriff11@users.noreply.github.com>
2024-04-15 15:40:55 +0100

    Add files via upload

---
+++ VitalityVaultAS/VitalityVault/app/src/main/java/com/example/vitalityvault/vvcal.java
@@ -0,0 +1,14 @@
+package com.example.vitalityvault;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vvcal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vvcal);
+    }
+}
